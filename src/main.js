062c59208daf515cded6b5caef73f73d6ceed58b
ty850454 <ty850454@126.com>
2018-03-20 22:03:26 +0800

    动态数据

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import FastClick from 'fastclick'
 import router from './router'
 import App from './App'
 import './assets/iconfont/iconfont.css'
+import { AjaxPlugin } from 'vux'
 
 FastClick.attach(document.body)
 
 Vue.config.productionTip = false
-
+Vue.use(AjaxPlugin)
 /* eslint-disable no-new */
 new Vue({
   router,
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <itinerarys>
-      <itinerary title="大理双廊二日骑纯玩苍山洱海索道" desc="大理三亚塔苍山索道洱海骑行" price="199" tag="丽江" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1521384422849&di=be733f171f44b430714cf0a673cadd0f&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F4a36acaf2edda3ccd53548ea0be93901203f9223.jpg"></itinerary>
-      <itinerary title="泰国曼谷芭提雅6天5晚奢华游" desc="0自费 豪华五行多城出发" price="3780" tag="芭提雅海滩" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1521384538682&di=110acd2abcd8da53c155bcf40cab527f&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F962bd40735fae6cdded3aa7505b30f2442a70fba.jpg"></itinerary>
-      <itinerary title="昆明大理丽江泸沽湖6天5晚三飞" desc="含三程机票+高档酒店" price="350" tag="香格里拉县" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1521981062&di=fc2872de9c087bbdebbbdc76e5d5b13a&imgtype=jpg&er=1&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dpixel_huitu%252C0%252C0%252C294%252C40%2Fsign%3D333876ccacefce1bfe26c08ac62996b2%2F810a19d8bc3eb135902d8281ad1ea8d3fd1f4425.jpg"></itinerary>
+      <itinerary v-for="itinerary in itinerarys"
+      :key="itinerary.id"
+      :title="itinerary.title"
+      :desc="itinerary.desc"
+      :price="itinerary.price"
+      :tag="itinerary.tag"
+      :src="itinerary.src"
+      />
     </itinerarys>
   </div>
 </template>
@@ -19,8 +24,16 @@ export default {
   },
   data () {
     return {
-      
+      itinerarys: []
     }
+  },
+  mounted: function () {
+    this.$nextTick(() => {
+      this.$http.get('./static/datas/luxian.json')
+        .then((response) => {
+          this.itinerarys = response.data
+        })
+    })
   }
 }
 </script>
